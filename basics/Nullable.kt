2bd2a55f3854d5998faea8796c976ab19f8e8e7f
aamir <16462830+batakpout@users.noreply.github.com>
2024-08-28 22:32:31 +0530

    Update Nullable.kt

--- basics/Nullable.kt
+++ basics/Nullable.kt
@@ -11,10 +11,13 @@ fun main() {
     /**
      * As the name suggests, if you use the !! not-null assertion,
      * it means that you assert that the value of the variable isn't null, regardless of whether it is or isn't.
-     * Unlike ?. safe-call operators, the use of a !! not-null assertion operator may result in a 
+     * Unlike ?. safe-call operators, the use of a !! not-null assertion operator may result in a
      * NullPointerException error being thrown if the nullable variable is indeed null
      */
     println(favoriteActor2!!.length)
+
+    //elvis operator
+   val res: Int =  favoriteActor2?.length ?: 0
 
 
 }
