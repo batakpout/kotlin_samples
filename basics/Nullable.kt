06f10f9766b1f67a784db037958ee9e0e30c1fe1
aamir <16462830+batakpout@users.noreply.github.com>
2024-08-28 22:28:57 +0530

    Update Nullable.kt

--- basics/Nullable.kt
+++ basics/Nullable.kt
@@ -4,4 +4,17 @@ fun main() {
 
     var favoriteActress: String = "Sandra Oh"
     //favoriteActress = null CTE
+
+    var favoriteActor2: String? = null
+    //println(favoriteActor2.length) CTE
+    println(favoriteActor2?.length)
+    /**
+     * As the name suggests, if you use the !! not-null assertion,
+     * it means that you assert that the value of the variable isn't null, regardless of whether it is or isn't.
+     * Unlike ?. safe-call operators, the use of a !! not-null assertion operator may result in a 
+     * NullPointerException error being thrown if the nullable variable is indeed null
+     */
+    println(favoriteActor2!!.length)
+
+
 }
